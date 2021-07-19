4ca7f5301af817045da1f7f895a95f94bf18cc46
Yuriy Fedyunkin <y.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.work>
2021-07-19 22:34:58 +0200

    Load button added with fetch JSON data logic and complition

--- Flight-Tickets/Flight-Tickets/MainViewController.m
+++ Flight-Tickets/Flight-Tickets/MainViewController.m
@@ -17,7 +17,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = UIColor.systemTealColor;
+    [self configureLoadButton];
 }
 
+- (void)configureLoadButton {
+    UIButton *loadButton = [[UIButton alloc] initWithFrame:
+                            CGRectMake(self.view.frame.size.width / 3,
+                                       self.view.frame.size.height / 2,
+                                       self.view.frame.size.width / 3,
+                                       44)];
+    loadButton.layer.masksToBounds = true;
+    loadButton.layer.cornerRadius = loadButton.frame.size.height / 2;
+    loadButton.backgroundColor = UIColor.darkGrayColor;
+    loadButton.tintColor = UIColor.whiteColor;
+    [loadButton setTitle:@"Load data" forState:normal];
+    [loadButton addTarget:self action:@selector(loadFlightsDataFromJSON) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:loadButton];
+}
+
+-(void) loadFlightsDataFromJSON {
+    [[DataManager sharedInstance] loadData];
+    self.view.backgroundColor = UIColor.cyanColor;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadDidComplete) name:kDataManagerLoadDataDidComplete object:nil];
+}
+
+- (void) loadDidComplete {
+    self.view.backgroundColor = UIColor.greenColor;
+}
+
+- (void) dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
 @end
