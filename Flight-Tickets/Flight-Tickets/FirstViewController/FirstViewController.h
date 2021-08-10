9df31103e920e35ecb37eaabd7b68f0f7841f369
Yuriy Fedyunkin <y.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.work>
2021-08-10 20:43:26 +0200

    FirstViewController class with onboarding logic added

---
+++ Flight-Tickets/Flight-Tickets/FirstViewController/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  Flight-Tickets
+//
+//  Created by Yuriy Fedyunkin on 10.08.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstViewController : UIPageViewController <UIPageViewControllerDelegate, UIPageViewControllerDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
