e32530970e9bcefa3d5a6ba3af8b9f3ef76d7a48
Yuriy Fedyunkin <y.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.worky.fedyunkin@upstarts.work>
2021-08-13 18:51:20 +0200

    NSString+Localize extension added

---
+++ Flight-Tickets/Flight-Tickets/Extensions/NSString+Localize.h
@@ -0,0 +1,18 @@
+//
+//  NSString+Localize.h
+//  Flight-Tickets
+//
+//  Created by Yuriy Fedyunkin on 13.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (Localize)
+
+@property (nonatomic, strong, readonly) NSString *localize;
+
+@end
+
+NS_ASSUME_NONNULL_END
